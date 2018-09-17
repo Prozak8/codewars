52fd99bf3aaa607e75dab186a50e9c44f9f35fde
Zak <zakary.howard94@gmail.com>
2018-09-17 09:53:22 +0200

    ReduceButGrow completed

---
+++ Ruby/Beginner - Reduce but Grow.rb
@@ -0,0 +1,18 @@
+# Given and array of integers (x), return the result of multiplying the values together in order. Example:
+
+# [1, 2, 3] --> 6
+# For the beginner, try to use the reduce method - it comes in very handy quite a lot so is a good one to know.
+
+# Array will not be empty.
+
+def grow(x)
+    x.map.reduce(:*)
+end
+
+def grow(x)
+    x.inject(:*)
+end
+
+def grow(x)
+    x.reduce(1, :*)
+end
