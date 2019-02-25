1ca849a19fe6c21ea79b2ce129b7801eef2be948
Zak <zakary.howard94@gmail.com>
2019-02-25 19:13:15 +0100

    Opposite Number (4)

---
+++ Javascript/8 kyu/OppositeNumber.js
@@ -0,0 +1,13 @@
+function opposite(number) {
+    return -number;
+}
+
+function opposite(number) {
+    return number * (-1);
+}
+
+function opposite(number) {
+    return number/-1
+}
+
+const opposite = number => -number;
